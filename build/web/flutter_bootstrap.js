ba1348708f755ff4e6b1bf2e959c7c2aa0c4569c
vistarlogitekdeveloper <Flutter.developer@vistarlogitek.com>
2026-06-27 12:03:30 +0530

    deploy: rebuild web bundle with latest changes2

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "520279924" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3169981996" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
